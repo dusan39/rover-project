169623678d96cbcbcfb46889451c4e28602833b7
Dusan <dusanrajkovic39@gmail.com>
2023-10-22 12:38:36 +0200

    Command and obstacle detection

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,54 @@ import { ref, computed } from 'vue';
     ['', '', '', ''],
   ]);
 
-  let roverComands = ref('');
+  let roverCommands = ref('');
   let roverPosition = ref('');
-  let comandsArray = [];
+  let commandsArray = [];
   let positionArray = [];
-  const isPositionSet = ref(false);
-  const roverX = ref(0); 
-  const roverY = ref(0); 
+
+  let isPositionSet = ref(false);
+  let disabled = ref(false);
+
+  let roverX = ref(0); 
+  let roverY = ref(0);
+  const obstacleX = ref(3);
+  const obstacleY = ref(3);
 
   const obstacle = (x, y) => {
-    return x === 0 && y === 0;
+    return x === obstacleX.value && y === obstacleY.value
   };
 
-  const addComand = () => {
-    const parts = roverComands.value.split('-');
-    comandsArray = comandsArray.concat(parts);
-    roverComands.value = '';
-    console.log(comandsArray)
+  const addCommand = () => {
+    const parts = roverCommands.value.split('-');
+    commandsArray = commandsArray.concat(parts);
+
+    function moveRover() {
+      if (commandsArray.length > 0) {
+        const command = commandsArray.shift();
+        switch (command) {
+          case 'n':
+            roverX.value = (roverX.value - 1 + 4) % 4;
+            break;
+          case 's':
+            roverX.value = (roverX.value + 1) % 4;
+            break;
+          case 'e':
+            roverY.value = (roverY.value + 1) % 4;
+            break;
+          case 'o':
+            roverY.value = (roverY.value - 1 + 4) % 4;
+            break;
+          default:
+            alert(`Invalid command: ${command}`);
+            break;
+        }
+        setTimeout(moveRover, 1000);
+      } else {
+        commandsArray = [];
+        roverCommands.value = '';
+      }
+    }
+    moveRover();
   };
 
   const addPosition = () => {
@@ -42,27 +73,47 @@ import { ref, computed } from 'vue';
     roverPosition.value = '';
   };
 
-  const shouldDisplayRover = computed(() => (x, y) => {
-    return x === roverX.value && y === roverY.value;
+  const shouldDisplayRover = computed(() => {
+    const roverAtObstacle = roverX.value === obstacleX.value && roverY.value === obstacleY.value;
+
+    if (roverAtObstacle) {
+      alert('There is an obstacle!');
+      retry()
+    }
+
+    return (x, y) => {
+      return !roverAtObstacle && x === roverX.value && y === roverY.value;
+    };
   });
+
+  const retry = () => {
+    roverX.value = null
+    roverY.value = null
+    commandsArray = [];
+    positionArray = [];
+    roverPosition.value = '';
+    roverCommands.value = '';
+    disabled.value = false
+    isPositionSet.value = false
+  }
 
 </script>
 
 <template>
   <div>
-    <main class="pt-8 text-center bg-gray-800 min-h-screen">
+    <main class="pt-8 text-center bg-[url('./assets/mars.jpg')] min-h-screen">
 
       <div class="flex justify-around">
         <div class="mb-10 flex justify-center flex-col">
           <input class="w-60 mx-auto mb-5" maxlength="3" v-model="roverPosition" placeholder="Enter rover's position" />
           <h3 class="mb-5 text-white">Example = 1-1</h3>
-          <button class="w-50 mx-auto border border-gray-100 text-xl px-10 py-5 rounded-2xl text-white" @click="addPosition" :disabled="roverPosition.trim() === ''">Set position</button>
+          <button class="w-50 mx-auto border border-gray-100 text-xl px-10 py-5 rounded-2xl text-white hover:bg-sky-700 transition ease-in-out delay-50 cursor-pointer" @click="addPosition" :disabled="roverPosition.trim() === '' || disabled === true">Set position</button>
         </div>
 
         <div class="mb-10 flex justify-center flex-col">
-          <input class="w-60 mx-auto mb-5" maxlength="9" v-model="roverComands" placeholder="Enter rover's comands" />
-          <h3 class="mb-5 text-white">Example = f-f-l-b-r</h3>
-          <button class="w-50 mx-auto border border-gray-100 text-xl px-10 py-5 rounded-2xl text-white" @click="addComand" :disabled="roverComands.trim() === ''">Submit comands</button>
+          <input class="w-60 mx-auto mb-5" maxlength="9" v-model="roverCommands" placeholder="Enter rover's comands" />
+          <h3 class="mb-5 text-white">Example = n-e-s-o-o</h3>
+          <button class="w-50 mx-auto border border-gray-100 text-xl px-10 py-5 rounded-2xl text-white hover:bg-sky-700 transition ease-in-out delay-50 cursor-pointer" @click="addCommand" :disabled="roverCommands.trim() === '' || disabled === true">Submit comands</button>
         </div>
       </div>
 
@@ -70,7 +121,7 @@ import { ref, computed } from 'vue';
       <div v-for="(row, x) in board" class="flex">
         <div v-for="(cell, y) in row" 
             :class="[
-              'border border-gray-800 w-20 h-20 flex items-center justify-center material-icons-outlined text-4xl dark:border-white',
+              'border border-white-800 w-20 h-20 flex items-center justify-center material-icons-outlined text-4xl',
               { 'bg-image-box': obstacle(x, y) }, { 'bg-image-box': shouldDisplayRover(x, y) }
             ]">
           <img v-if="obstacle(x, y)" src="./assets/obstacle.svg" alt="Obstacle" class="w-14 h-14">
@@ -78,8 +129,6 @@ import { ref, computed } from 'vue';
         </div>
       </div>
     </div>
-
-    <button class="w-40 mx-auto border border-gray-100 text-xl px-10 py-5 rounded-2xl text-white">Retry</button>
 
     </main>  
   </div>
